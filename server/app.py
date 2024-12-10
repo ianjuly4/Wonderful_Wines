6cd1c662047271181974d51c49cbbae4921a14a6
Ian July <ianjuly4@gmail.com>
2024-12-09 20:45:21 -0700

    models update

--- server/app.py
+++ server/app.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import request, session, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from config import app, db, Api, CORS, Migrate
+from models import Wine, Review, User
 
-# Local imports
-from config import app, db, api
-# Add your model imports
 
+CORS(app, origins='http://localhost:4000')
+migrate = Migrate(app, db)
+api = Api(app)
 
-# Views go here!
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -1,12 +1,8 @@
-
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-db = SQLAlchemy()
-from flask_restful import Resource
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
 
-from config import db
+from config import db, bcrypt, association_proxy
 
 
 class Wine(db.Model, SerializerMixin):
@@ -14,7 +10,6 @@ class Wine(db.Model, SerializerMixin):
 
     serialize_rules = ('-reviews.wine',)
     
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     type = db.Column(db.String)
@@ -46,12 +41,31 @@ class Review(db.Model, SerializerMixin):
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    serialize_rules = ('-reviews.user',)
-
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash = db.Column(db.String)
-    
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
     reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
     
     wines = association_proxy('reviews', 'wine', creator=lambda wine_obj: Review(wine=wine_obj))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username cannot be null")
+        return username
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
