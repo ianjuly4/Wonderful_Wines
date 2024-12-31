8a673f7fdad5233ffed58fec68a4db91ce4a9553
ianjuly4 <ianjuly4@gmail.com>
2024-12-31 00:46:16 -0700

    sqlalchemy in config update

--- server/config.py
+++ server/config.py
@@ -1,4 +1,3 @@
-#/server/config.py
 import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
@@ -7,11 +6,11 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.ext.associationproxy import association_proxy
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
+# Initialize the Flask app
 app = Flask(
     __name__,
     static_url_path='',
@@ -19,26 +18,30 @@ app = Flask(
     template_folder='../client/build'
 )
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///instance/app.db')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', b'9\x8c_\xc8\x16$OE\xbe5\x98d\x19\xc1\xd8(')  
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', b'9\x8c_\xc8\x16$OE\xbe5\x98d\x19\xc1\xd8(')
 app.config['SESSION_COOKIE_NAME'] = 'session' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_COOKIE_SECURE'] = True  
 app.config['SESSION_COOKIE_HTTPONLY'] = True  
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  
-
 app.json.compact = False
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
-
 CORS(app, origins=['http://localhost:3000', 'https://wonderful-wines-1.onrender.com'], supports_credentials=True)
-
 api = Api(app)
+
+db.init_app(app)
+
 
 __all__ = ["app", "db", "bcrypt", "migrate", "api", "os"]
