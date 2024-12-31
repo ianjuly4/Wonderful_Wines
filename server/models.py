e3276d5dd91416157b708a93ab8858d16487c7b9
ianjuly4 <ianjuly4@gmail.com>
2024-12-31 14:17:36 -0700

    login update

--- server/models.py
+++ server/models.py
@@ -43,23 +43,18 @@ class User(db.Model, SerializerMixin):
     username = db.Column(db.String, unique=True)
     _password_hash = db.Column(db.String, nullable=False)
 
-    reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
-    wines = association_proxy('reviews', 'wine', creator=lambda wine_obj: Review(wine=wine_obj))
-
-    @validates('username')
-    def validate_username(self, key, username):
-        if not username:
-            raise ValueError("Username cannot be null")
-        return username
+    articles = db.relationship('Article', backref='user')
 
     @hybrid_property
     def password_hash(self):
-        raise AttributeError('Password hashes may not be viewed.')
+        return self._password_hash
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
